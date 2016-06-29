a9fe4f7d559ac66db37d72545ef0b09f4ccc4ce1
Jia Deng <flyucarl@gmail.com>
2016-06-30 00:17:00 +0800

    my smartparens key!

--- personal/personal.el
+++ personal/personal.el
@@ -1,10 +1,14 @@
+;;; package -- Summary
+;;; Commentary:
+;;; Code:
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier 'super)
 
 (sp-local-pair 'clojure-mode "`" "`")
 
-(prelude-require-packages '(evil-smartparens ag))
+(prelude-require-packages '(evil-smartparens  bind-key ag))
 (add-hook 'smartparens-enabled-hook #'evil-smartparens-mode)
+(setq ag-highlight-search t)
 
 (provide 'personal)
 ;;; personal.el ends here
